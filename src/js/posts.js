45112f3f5302bb2179174b1f166c09306700d920
markuslem <markus.lemberg@ut.ee>
2024-11-05 11:09:17 +0200

    Author name is displayed under posts

--- src/js/posts.js
+++ src/js/posts.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+window.onload = function () {
 
 
 
     fetch('https://api.npoint.io/b42e30b4f83c6842308a?fbclid=IwY2xjawGSMHVleHRuA2FlbQIxMAABHRWFCcVB9jkWyqart5QP-moNU8Dg-Zdraco_zDv5lLcaW1AF1ma14jfdZQ_aem_B3HqGcwEJyzboQXKWQlVTQ')
-    // fetch('res/json/posts.json')
+        // fetch('res/json/posts.json')
         .then((response) => response.json())
         .then(json => {
             console.log(json);
@@ -11,54 +11,66 @@ window.onload = function() {
             let posts = json.posts;
             let divpage = document.getElementsByClassName("content-container")[0];
 
-            
 
-            for (let i = 0; i < posts.length; i++){
 
-            let div = document.createElement("div");
-            div.className = 'content-box';
+            for (let i = 0; i < posts.length; i++) {
 
-            let div1 = document.createElement("div");
-            let p = document.createElement("p");
-            p.innerText=posts[i]["create-time"];
-            div1.appendChild(p);
-            let img = document.createElement("img");
-            img.src="res/pics/me.png";
-            img.alt="Profile Icon";
-            img.width = 30;
-            img.height = 30;
-            div1.appendChild(img);
-            div.appendChild(div1);
+                const post = posts[i];
+                let div = document.createElement("div");
+                div.className = 'content-box';
 
-            if (posts[i].picture){
-                let img1 = document.createElement("img");
-                img1.src = posts[i].picture;
-                img1.style.width = "100%";
-            
-                let p1 = document.createElement("p");
-                p1.innerText=posts[i]["picture-info"];
-                div.appendChild(img1);
-                div.appendChild(p1);
+                // Post creation time, author and profile picture
+                let postHeaderDiv = document.createElement("div");
+                let p = document.createElement("p");
+                p.innerText = post["create-time"];
+                postHeaderDiv.appendChild(p);
+                
+                let imgAndAuthorDiv = document.createElement("div");
+                let img = document.createElement("img");
+                img.src = "res/pics/me.png";
+                img.alt = "Profile Icon";
+                img.width = 30;
+                img.height = 30;
+                imgAndAuthorDiv.appendChild(img);
+
+                let author = document.createElement("p");
+                author.innerText = post["author"];
+                imgAndAuthorDiv.appendChild(author);
+
+                postHeaderDiv.appendChild(imgAndAuthorDiv);
+                div.appendChild(postHeaderDiv);
+
+                // Post content
+                if (post.picture) {
+                    let img1 = document.createElement("img");
+                    img1.src = post.picture;
+                    img1.style.width = "100%";
+
+                    let p1 = document.createElement("p");
+                    p1.innerText = post["picture-info"];
+                    div.appendChild(img1);
+                    div.appendChild(p1);
+                }
+
+                if (post.desc) {
+                    let p2 = document.createElement("p");
+                    p2.innerText = post.desc;
+                    div.appendChild(p2);
+                }
+
+                // Like count
+                let p3 = document.createElement("p");
+                p3.innerText = "🖤 " + post["like-count"];
+                div.appendChild(p3);
+
+
+
+                divpage.appendChild(div);
+
+
+
             }
 
-            if (posts[i].desc){
-                let p2 = document.createElement("p");
-                p2.innerText=posts[i].desc;
-                div.appendChild(p2);
-            }
-
-            let p3 = document.createElement("p");
-            p3.innerText = "🖤 " + posts[i]["like-count"];
-            div.appendChild(p3);
-
-            
-
-            divpage.appendChild(div);
-
-    
-
-            }
-        
 
         })
 
